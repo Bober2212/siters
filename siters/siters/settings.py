bd33093a3b27759f4c519c8e7e9bdc68f9f31805
Your Name <you@example.com>
2023-12-29 19:40:41 +0200

    ok

--- siters/siters/settings.py
+++ siters/siters/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_siters.apps.SitersAppConfig'
+    'app_siters.apps.AppSitersConfig'
 ]
 
 MIDDLEWARE = [
